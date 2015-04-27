4220ffa48160c59ae1882dbc88fb284d446b161d
Jeremy Scheff <jdscheff@gmail.com>
2015-04-27 00:45:14 -0400

    Not quite complete refactor of index.js. This one test fails, plus some of the skipped ones have not yet been checked.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var Database = require('./lib/Database');
 var FDBOpenDBRequest = require('./lib/FDBOpenDBRequest');
 var FDBDatabase = require('./lib/FDBDatabase');
 var FDBVersionChangeEvent = require('./lib/FDBVersionChangeEvent');
+var VersionError = require('./lib/errors/VersionError');
 var cmp = require('./lib/cmp');
 
 var databases = {};
@@ -17,46 +18,89 @@ function fireOpenSuccessEvent(request, db) {
     request.dispatchEvent(event);
 }
 
-// http://www.w3.org/TR/IndexedDB/#dfn-steps-for-opening-a-database
-function openDatabase(name, version, request) {
-    if (!databases.hasOwnProperty(name)) {
-        databases[name] = new Database(name, version);
-        var db = new FDBDatabase(databases[name]);
-        request.result = db; // for versionchange
+function runVersionchangeTransaction(connection, version, request, cb) {
+    var oldVersion = connection.version;
 
-        try {
-            request.transaction = db.transaction(db.objectStoreNames, 'versionchange');
-            request.transaction.addEventListener('complete', function () {
-                request.transaction = null;
+//  Set the version of database to version. This change is considered part of the transaction, and so if the transaction is aborted, this change is reverted. 
+    connection._database.version = version;
+    connection.version = version;
 
-                setImmediate(fireOpenSuccessEvent.bind(null, request, db));
-            });
-            request.transaction.addEventListener('error', function (e) {
+    setImmediate(function () {
+        var transaction = connection.transaction(connection.objectStoreNames, 'versionchange');
+        request.result = connection;
+        request.transaction = transaction;
+
+        var event = new FDBVersionChangeEvent();
+        event.target = request;
+        event.type = 'upgradeneeded';
+        event.oldVersion = oldVersion;
+        event.newVersion = version;
+        request.dispatchEvent(event);
+
+        request.readyState = 'done';
+
+        transaction.addEventListener('error', function (e) {
+console.log('error in versionchange transaction - should abort here probably')
 // Ugly hack so it runs after all other tx stuff finishes. Need a real queue, or a more appropriate time to schedule
-                setTimeout(function () {
-                    request.error = new Error();
-                    request.error.name = e.target.error.name;
-                    var event = new Event('error', {
-                        bubbles: true,
-                        cancelable: false
-                    });
-                    event._eventPath = [];
-                    request.dispatchEvent(event);
-                }, 1);
+            /*setTimeout(function () {
+                request.error = new Error();
+                request.error.name = e.target.error.name;
+                var event = new Event('error', {
+                    bubbles: true,
+                    cancelable: false
+                });
+                event._eventPath = [];
+                request.dispatchEvent(event);
+            }, 1);*/
+        });
+        transaction.addEventListener('abort', function () {
+            request.transaction = null;
+console.log('abort in versionchange transaction')
+        });
+        transaction.addEventListener('complete', function () {
+            request.transaction = null;
+            // Let other complete event handlers run before continuing
+            setImmediate(function () {
+                cb(null);
             });
+        });
+    });
+}
 
-            var event = new FDBVersionChangeEvent();
-            event.target = request;
-            event.type = 'upgradeneeded';
-            request.dispatchEvent(event);
-        } catch (err) {
-            if (request.transaction) {
-                request.transaction._abort('AbortError');
-            }
-            throw err;
-        }
+// http://www.w3.org/TR/IndexedDB/#dfn-steps-for-opening-a-database
+function openDatabase(name, version, request, cb) {
+    var db;
+    if (databases.hasOwnProperty(name)) {
+        db = databases[name];
     } else {
-        fireOpenSuccessEvent(request, new FDBDatabase(databases[name]));
+        db = new Database(name, 0);
+        databases[name] = db;
+    }
+
+    if (version === undefined) {
+        version = 1;
+    }
+
+    if (db.version > version) {
+        throw new VersionError();
+    }
+
+    var connection = new FDBDatabase(databases[name]);
+
+    if (db.version < version) {
+        runVersionchangeTransaction(connection, version, request, function (err) {
+            if (err) {
+// If the previous step resulted in an error, then return that error and abort these steps. If the "versionchange" transaction in the previous step was aborted, or if connection is closed, return a DOMError of type AbortError and abort these steps. In either of these cases, ensure that connection is closed by running the steps for closing a database connection before these steps are aborted.
+/*                if (request.transaction) {
+                    request.transaction._abort('AbortError');
+                }*/
+                return cb(err);
+            }
+
+            cb(null, connection);
+        });
+    } else {
+        cb(null, connection);
     }
 }
 
@@ -74,19 +118,23 @@ fakeIndexedDB.open = function (name, version) {
     request.source = null;
 
     setImmediate(function () {
-        try {
-            openDatabase(name, version, request);
-        } catch (err) {
-            request.error = new Error();
-            request.error.name = err.name;
+        openDatabase(name, version, request, function (err, connection) {
+            if (err) {
+                request.error = new Error();
+                request.error.name = err.name;
 
-            var event = new Event('error', {
-                bubbles: true,
-                cancelable: false
-            });
-            event._eventPath = [];
-            request.dispatchEvent(event);
-        }
+                var event = new Event('error', {
+                    bubbles: true,
+                    cancelable: false
+                });
+                event._eventPath = [];
+                request.dispatchEvent(event);
+
+                return;
+            }
+
+            fireOpenSuccessEvent(request, connection);
+        });
     });
 
     return request;
