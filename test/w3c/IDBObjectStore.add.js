7becb8199a0d95988925db28a4fd8bf3f65c2e11
Jeremy Scheff <jdscheff@gmail.com>
2015-04-27 21:43:21 -0400

    IDBObjectStore.add tests

--- test/w3c/IDBObjectStore.add.js
+++ test/w3c/IDBObjectStore.add.js
@@ -6,9 +6,9 @@ var ReadOnlyError = require('../../lib/errors/ReadOnlyError');
 var support = require('./support');
 var createdb = support.createdb;
 
-describe('W3C IDBObjectStore.put Tests', function () {
-    // idbobjectstore_put
-    it('put with an inline key', function (done) {
+describe('W3C IDBObjectStore.add Tests', function () {
+    // idbobjectstore_add
+    it('add with an inline key', function (done) {
         var db,
           record = { key: 1, property: "data" };
 
@@ -17,7 +17,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { keyPath: "key" });
 
-            objStore.put(record);
+            objStore.add(record);
         };
 
         open_rq.onsuccess = function(e) {
@@ -33,8 +33,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         };
     });
 
-    // idbobjectstore_put2
-    it('put with an out-of-line key', function (done) {
+    // idbobjectstore_add2
+    it('add with an out-of-line key', function (done) {
         var db,
           key = 1,
           record = { property: "data" };
@@ -44,7 +44,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
             db = e.target.result;
             var objStore = db.createObjectStore("store");
 
-            objStore.put(record, key);
+            objStore.add(record, key);
         };
 
         open_rq.onsuccess = function(e) {
@@ -60,65 +60,54 @@ describe('W3C IDBObjectStore.put Tests', function () {
         };
     });
 
-    // idbobjectstore_put3
+    // idbobjectstore_add3
     it('record with same key already exists', function (done) {
-        var db, success_event,
-          record = { key: 1, property: "data" },
-          record_put = { key: 1, property: "changed", more: ["stuff", 2] };
+        var db,
+          record = { key: 1, property: "data" };
 
         var open_rq = createdb(done);
         open_rq.onupgradeneeded = function(e) {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { keyPath: "key" });
-            objStore.put(record);
+            objStore.add(record);
 
-            var rq = objStore.put(record_put);
+            var rq = objStore.add(record);
+            rq.onsuccess = function () { throw new Error("success on adding duplicate record"); };
 
-            rq.onsuccess = function(e) {
-                success_event = true;
+            rq.onerror = function(e) {
+                assert.equal(e.target.error.name, "ConstraintError");
+                assert.equal(rq.error.name, "ConstraintError");
+                assert.equal(e.type, "error");
+
+                e.preventDefault();
+                e.stopPropagation();
             };
         };
 
+        // Defer done, giving rq.onsuccess a chance to run
         open_rq.onsuccess = function(e) {
-            assert(success_event);
-
-            var rq = db.transaction("store")
-                       .objectStore("store")
-                       .get(1);
-
-            rq.onsuccess = function(e) {
-                var rec = e.target.result;
-
-                assert.equal(rec.key, record_put.key);
-                assert.equal(rec.property, record_put.property);
-                assert.deepEqual(rec.more, record_put.more);
-
-                done();
-            };
-        };
+            done();
+        }
     });
 
-    // idbobjectstore_put4
-    it('put where an index has unique:true specified', function (done) {
+    // idbobjectstore_add4
+    it('add where an index has unique:true specified', function (done) {
         var db,
           record = { key: 1, property: "data" };
-        var errors = 0;
 
         var open_rq = createdb(done);
         open_rq.onupgradeneeded = function(e) {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { autoIncrement: true });
             objStore.createIndex("i1", "property", { unique: true });
-            objStore.put(record);
+            objStore.add(record);
 
-            var rq = objStore.put(record);
+            var rq = objStore.add(record);
+            rq.onsuccess = function () { throw new Error("success on adding duplicate indexed record"); };
 
             rq.onerror = function(e) {
-                errors += 1;
-
                 assert.equal(rq.error.name, "ConstraintError");
                 assert.equal(e.target.error.name, "ConstraintError");
-
                 assert.equal(e.type, "error");
 
                 e.preventDefault();
@@ -128,12 +117,11 @@ describe('W3C IDBObjectStore.put Tests', function () {
 
         // Defer done, giving a spurious rq.onsuccess a chance to run
         open_rq.onsuccess = function(e) {
-            assert.equal(errors, 1);
             done();
         }
     });
 
-    // idbobjectstore_put5
+    // idbobjectstore_add5
     it("object store's key path is an object attribute", function (done) {
         var db,
           record = { test: { obj: { key: 1 } }, property: "data" };
@@ -142,7 +130,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onupgradeneeded = function(e) {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { keyPath: "test.obj.key" });
-            objStore.put(record);
+            objStore.add(record);
         };
 
         open_rq.onsuccess = function(e) {
@@ -158,7 +146,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
         };
     });
 
-    // idbobjectstore_put6
+    // idbobjectstore_add6
     it('autoIncrement and inline keys', function (done) {
         var db,
           record = { property: "data" },
@@ -169,10 +157,10 @@ describe('W3C IDBObjectStore.put Tests', function () {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { keyPath: "key", autoIncrement: true });
 
-            objStore.put(record);
-            objStore.put(record);
-            objStore.put(record);
-            objStore.put(record);
+            objStore.add(record);
+            objStore.add(record);
+            objStore.add(record);
+            objStore.add(record);
         };
 
         open_rq.onsuccess = function(e) {
@@ -196,7 +184,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
         };
     });
 
-    // idbobjectstore_put7
+    // idbobjectstore_add7
     it('autoIncrement and out-of-line keys', function (done) {
         var db,
           record = { property: "data" },
@@ -207,10 +195,10 @@ describe('W3C IDBObjectStore.put Tests', function () {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { autoIncrement: true });
 
-            objStore.put(record);
-            objStore.put(record);
-            objStore.put(record);
-            objStore.put(record);
+            objStore.add(record);
+            objStore.add(record);
+            objStore.add(record);
+            objStore.add(record);
         };
 
         open_rq.onsuccess = function(e) {
@@ -234,7 +222,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
         };
     });
 
-    // idbobjectstore_put8
+    // idbobjectstore_add8
     it('object store has autoIncrement:true and the key path is an object attribute', function (done) {
         var db,
           record = { property: "data" },
@@ -245,10 +233,10 @@ describe('W3C IDBObjectStore.put Tests', function () {
             db = e.target.result;
             var objStore = db.createObjectStore("store", { keyPath: "test.obj.key", autoIncrement: true });
 
-            objStore.put(record);
-            objStore.put(record);
-            objStore.put(record);
-            objStore.put(record);
+            objStore.add(record);
+            objStore.add(record);
+            objStore.add(record);
+            objStore.add(record);
         };
 
         open_rq.onsuccess = function(e) {
@@ -272,8 +260,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         };
     });
 
-    // idbobjectstore_put9
-    it("Attempt to put a record that does not meet the constraints of an object store's inline key requirements", function (done) {
+    // idbobjectstore_add9
+    it("Attempt to add a record that does not meet the constraints of an object store's inline key requirements", function (done) {
         var record = { key: 1, property: "data" };
 
         var open_rq = createdb(done);
@@ -283,7 +271,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
               objStore = db.createObjectStore("store", { keyPath: "key" });
 
             assert.throws(function() {
-                rq = objStore.put(record, 1);
+                rq = objStore.add(record, 1);
             }, DataError);
 
             assert.equal(rq, undefined);
@@ -292,8 +280,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onsuccess = function () {};
     });
 
-    // idbobjectstore_put10
-    it("Attempt to call 'put' without an key parameter when the object store uses out-of-line keys", function (done) {
+    // idbobjectstore_add10
+    it("Attempt to call 'add' without an key parameter when the object store uses out-of-line keys", function (done) {
         var db,
           record = { property: "data" };
 
@@ -305,7 +293,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
               objStore = db.createObjectStore("store", { keyPath: "key" });
 
             assert.throws(function() {
-                rq = objStore.put(record);
+                rq = objStore.add(record);
             }, DataError);
 
             assert.equal(rq, undefined);
@@ -314,8 +302,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onsuccess = function () {};
     });
 
-    // idbobjectstore_put11
-    it("Attempt to put a record where the record's key does not meet the constraints of a valid key", function (done) {
+    // idbobjectstore_add11
+    it("Attempt to add a record where the record's key does not meet the constraints of a valid key", function (done) {
         var db,
           record = { key: { value: 1 }, property: "data" };
 
@@ -327,7 +315,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
               objStore = db.createObjectStore("store", { keyPath: "key" });
 
             assert.throws(function() {
-                rq = objStore.put(record);
+                rq = objStore.add(record);
             }, DataError);
 
             assert.equal(rq, undefined);
@@ -336,8 +324,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onsuccess = function () {};
     });
 
-    // idbobjectstore_put12
-    it("Attempt to put a record where the record's in-line key is not defined", function (done) {
+    // idbobjectstore_add12
+    it("Attempt to add a record where the record's in-line key is not defined", function (done) {
         var db,
           record = { property: "data" };
 
@@ -349,7 +337,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
               objStore = db.createObjectStore("store", { keyPath: "key" });
 
             assert.throws(function() {
-                rq = objStore.put(record);
+                rq = objStore.add(record);
             }, DataError);
 
             assert.equal(rq, undefined);
@@ -358,8 +346,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onsuccess = function () {};
     });
 
-    // idbobjectstore_put13
-    it('Attempt to put a record where the out of line key provided does not meet the constraints of a valid key', function (done) {
+    // idbobjectstore_add13
+    it('Attempt to add a record where the out of line key provided does not meet the constraints of a valid key', function (done) {
         var db,
           record = { property: "data" };
 
@@ -371,7 +359,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
               objStore = db.createObjectStore("store");
 
             assert.throws(function() {
-                rq = objStore.put(record, { value: 1 });
+                rq = objStore.add(record, { value: 1 });
             }, DataError);
 
             assert.equal(rq, undefined);
@@ -380,8 +368,8 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onsuccess = function () {};
     });
 
-    // idbobjectstore_put14
-    it('Put a record where a value being indexed does not meet the constraints of a valid key', function (done) {
+    // idbobjectstore_add14
+    it('add a record where a value being indexed does not meet the constraints of a valid key', function (done) {
         var db,
           record = { key: 1, indexedProperty: { property: "data" } };
 
@@ -394,7 +382,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
 
             objStore.createIndex("index", "indexedProperty");
 
-            rq = objStore.put(record);
+            rq = objStore.add(record);
 
             assert(rq instanceof FDBRequest);
             rq.onsuccess = function() {
@@ -404,7 +392,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
         open_rq.onsuccess = function () {};
     });
 
-    // idbobjectstore_put15
+    // idbobjectstore_add15
     it('If the transaction this IDBObjectStore belongs to has its mode set to readonly, throw ReadOnlyError', function (done) {
         var db;
 
@@ -418,13 +406,13 @@ describe('W3C IDBObjectStore.put Tests', function () {
             var txn = db.transaction("store");
             var ostore = txn.objectStore("store");
             assert.throws(function() {
-                ostore.put({pKey: "primaryKey_0"});
+                ostore.add({pKey: "primaryKey_0"});
             }, ReadOnlyError);
             done();
         }
     });
 
-    // idbobjectstore_put16
+    // idbobjectstore_add16
     it.skip('If the object store has been deleted, the implementation must throw a DOMException of type InvalidStateError', function (done) {
         var db,
             ostore;
@@ -438,7 +426,7 @@ describe('W3C IDBObjectStore.put Tests', function () {
 
         open_rq.onsuccess = function (event) {
             assert.throws(function() {
-                ostore.put({pKey: "primaryKey_0"});
+                ostore.add({pKey: "primaryKey_0"});
             }, InvalidStateError);
             done();
         }
