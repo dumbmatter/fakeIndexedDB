546de93779d00a5836643ec6b59dc1d708256d21
Jeremy Scheff <jdscheff@gmail.com>
2015-04-28 00:35:24 -0400

    Added request bubbling and capturing test

--- test/w3c/TransactionBehavior.js
+++ test/w3c/TransactionBehavior.js
@@ -1,8 +1,4 @@
 var assert = require('assert');
-//var indexedDB = require('../..');
-//var FDBOpenDBRequest = require('../../lib/FDBOpenDBRequest');
-//var FDBTransaction = require('../../lib/FDBTransaction');
-//var InvalidStateError = require('../../lib/errors/InvalidStateError');
 var support = require('./support');
 var createdb = support.createdb;
 
