4f54db8f2c4d6159d98053903e515d7e8d1c5a3d
Jeremy Scheff <jdscheff@gmail.com>
2015-05-01 22:07:13 -0400

    IDBTransaction.abort test somehow works

--- test/w3c/IDBTransaction.abort.js
+++ test/w3c/IDBTransaction.abort.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+var FDBIndex = require('../../lib/FDBIndex');
 var support = require('./support');
 var createdb = support.createdb;
 
@@ -28,5 +29,37 @@ describe('W3C IDBTransaction.abort Tests', function () {
             assert.equal(open_rq.transaction, null);
             done();
         }
+    });
+
+    // idbtransaction_abort
+    it('IDBTransaction - abort', function (done) {
+        var db, aborted,
+          record = { indexedProperty: "bar" };
+
+        var open_rq = createdb(done);
+        open_rq.onupgradeneeded = function(e) {
+            db = e.target.result;
+            var txn = e.target.transaction,
+              objStore = db.createObjectStore("store");
+
+            objStore.add(record, 1);
+            objStore.add(record, 2);
+            var index = objStore.createIndex("index", "indexedProperty", { unique: true });
+
+            assert(index instanceof FDBIndex, "IDBIndex");
+
+            e.target.transaction.onabort = function(e) {
+                aborted = true;
+                assert.equal(e.type, "abort", "event type");
+            };
+
+            db.onabort = function(e) {
+                assert(aborted, "transaction.abort event has fired");
+                done();
+            };
+
+            e.target.transaction.oncomplete = function () { throw new Error("got complete, expected abort"); };
+        };
+        open_rq.onerror = function (e) {};
     });
 });
