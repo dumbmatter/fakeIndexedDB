cb9e29fcc1c115d3ceef5c109b311b856acb9fa0
Jeremy Scheff <jdscheff@gmail.com>
2019-11-15 11:22:17 -0500

    Tests to ensure imports actually work

---
+++ test/test.js
@@ -0,0 +1,14 @@
+// Sanity check that imports work
+
+require("../build");
+require("../lib/FDBCursor");
+require("../lib/FDBCursorWithValue");
+require("../lib/FDBDatabase");
+require("../lib/FDBFactory");
+require("../lib/FDBIndex");
+require("../lib/FDBKeyRange");
+require("../lib/FDBObjectStore");
+require("../lib/FDBOpenDBRequest");
+require("../lib/FDBRequest");
+require("../lib/FDBTransaction");
+require("../lib/FDBVersionChangeEvent");
