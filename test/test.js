618285df00d7a25a902d4f9881cfda4e5c581f14
Jeremy Scheff <jdscheff@gmail.com>
2021-07-12 16:59:37 -0400

    Types, not really working

--- test/test.js
+++ test/test.js
@@ -1,14 +1,16 @@
 // Sanity check that imports work
 
-import "../build/index.js";
-/*import "../lib/FDBCursor.js";
-import "../lib/FDBCursorWithValue.js";
-import "../lib/FDBDatabase.js";
-import "../lib/FDBFactory.js";
-import "../lib/FDBIndex.js";
-import "../lib/FDBKeyRange.js";
-import "../lib/FDBObjectStore.js";
-import "../lib/FDBOpenDBRequest.js";
-import "../lib/FDBRequest.js";
-import "../lib/FDBTransaction.js";
-import "../lib/FDBVersionChangeEvent.js";*/
+import {
+    indexedDB,
+    IDBCursor,
+    IDBCursorWithValue,
+    IDBDatabase,
+    IDBFactory,
+    IDBIndex,
+    IDBKeyRange,
+    IDBObjectStore,
+    IDBOpenDBRequest,
+    IDBRequest,
+    IDBTransaction,
+    IDBVersionChangeEvent,
+} from "../build/index.js";
