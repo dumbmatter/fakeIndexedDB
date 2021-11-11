3e02a7b9c69522e0bd8e41866aa3cbf6e223000c
Jeremy Scheff <jdscheff@gmail.com>
2021-11-11 16:28:39 -0500

    Use built-in structuredClone function when possible

--- src/lib/structuredClone.ts
+++ src/lib/structuredClone.ts
@@ -1,8 +1,16 @@
+// Built-in structuredClone arrived in Node 17, so we need to keep this file around as long as we support Node 16
+
 // @ts-ignore
 import realisticStructuredClone from "realistic-structured-clone";
 import { DataCloneError } from "./errors.js";
 
-const structuredClone = <T>(input: T): T => {
+const structuredCloneWrapper = <T>(input: T): T => {
+    // @ts-ignore
+    if (typeof structuredClone !== "undefined") {
+        // @ts-ignore
+        return structuredClone(input);
+    }
+
     try {
         return realisticStructuredClone(input);
     } catch (err) {
@@ -10,4 +18,4 @@ const structuredClone = <T>(input: T): T => {
     }
 };
 
-export default structuredClone;
+export default structuredCloneWrapper;
