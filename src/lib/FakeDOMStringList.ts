a3723a93ac4689eaea10268718b39a299424f827
Jeremy Scheff <jdscheff@gmail.com>
2025-05-15 17:03:22 -0400

    Simpler than a proxy, works well enough, and also supports the old phantomjs tests. Proxy might be slightly more correct for things like Object.getOwnPropertyDescriptor and Object.hasOwn

--- src/lib/FakeDOMStringList.ts
+++ src/lib/FakeDOMStringList.ts
@@ -1,48 +1,12 @@
-const regex = /^\d+$/;
-const isPositiveInteger = (prop: unknown): prop is string => {
-    return typeof prop === "string" && regex.test(prop);
-};
-
 class FakeDOMStringList implements DOMStringList {
     private _values: string[];
 
     constructor(...values: string[]) {
         this._values = values;
 
-        return new Proxy(this, {
-            get(target, prop) {
-                if (isPositiveInteger(prop)) {
-                    const index = parseInt(prop);
-                    return target._values[index];
-                }
-                return (target as any)[prop];
-            },
-            has(target, prop) {
-                if (isPositiveInteger(prop)) {
-                    const index = parseInt(prop);
-                    return index < target._values.length;
-                }
-                return prop in target;
-            },
-            ownKeys(target) {
-                return Array.from({ length: target._values.length }, (_, i) =>
-                    String(i),
-                );
-            },
-            getOwnPropertyDescriptor(target, prop) {
-                if (isPositiveInteger(prop)) {
-                    const index = parseInt(prop);
-                    if (index < target._values.length) {
-                        return {
-                            value: target._values[index],
-                            enumerable: true,
-                            configurable: true,
-                            writable: false,
-                        };
-                    }
-                }
-            },
-        });
+        for (let i = 0; i < values.length; i++) {
+            this[i] = values[i];
+        }
     }
 
     contains(value: string) {
@@ -69,10 +33,20 @@ class FakeDOMStringList implements DOMStringList {
 
     // Used internally, should not be used by others. I could maybe get rid of these and replace rather than mutate, but too lazy to check the spec.
     _push(...values: Parameters<typeof Array.prototype.push>) {
-        return this._values.push(...values);
+        for (let i = 0; i < values.length; i++) {
+            this[this._values.length + i] = values[i];
+        }
+
+        this._values.push(...values);
     }
     _sort(...values: Parameters<typeof Array.prototype.sort>) {
-        return this._values.sort(...values);
+        this._values.sort(...values);
+
+        for (let i = 0; i < this._values.length; i++) {
+            this[i] = this._values[i];
+        }
+
+        return this;
     }
 }
 
